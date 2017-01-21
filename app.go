93bb1de41e05578ceb39eceedab4e11c747655b9
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-01-21 18:41:05 -0200

    Updated app.go.

--- app.go
+++ app.go
@@ -2,13 +2,17 @@ package main
 
 import (
     _ "./endpoints"
+    "./utils/mongodb"
     "log"
     "net/http"
 )
 
 func main(){
+    session := mongodb.Start()
+    defer session.Close()
+
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
-        log.Fatal("ListenAndServe: ", err)
+        log.Fatal("Error while listenning to port 8080: ", err)
     }
 }
