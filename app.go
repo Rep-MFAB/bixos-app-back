a3544ad6f89eca5ba6a901ccf3f2b20f5dd8e3c5
Marco Steck <steckmarco@outlook.com>
2017-02-01 14:19:26 -0200

    Formatted with gofmt.

--- app.go
+++ app.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-    _ "./endpoints"
-    "./config"
-    "./utils/mongodb"
-    "net/http"
-    "log"
-    "fmt"
+	"fmt"
+	"log"
+	"net/http"
+
+	"./config"
+	_ "./endpoints"
+	"./utils/mongodb"
 )
 
-func main(){
-    session := mongodb.Start()
-    defer session.Close()
+func main() {
+	session := mongodb.Start()
+	defer session.Close()
 
-    log.Printf("Serving app on host: \033[95mlocalhost:%d\033[0m", config.Config.Server.Port)
-    err := http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Server.Port), nil)
-    if err != nil {
-        log.Fatal(fmt.Sprintf("Error while listenning to port %d: ", config.Config.Server.Port), err)
-    }
+	log.Printf("Serving app on host: \033[95mlocalhost:%d\033[0m", config.Config.Server.Port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Server.Port), nil)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Error while listenning to port %d: ", config.Config.Server.Port), err)
+	}
 
 }
