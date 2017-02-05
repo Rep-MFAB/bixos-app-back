603bd19f691e1c8f3e0bdf3156d5fe0b3b0b3527
Marco Steck <steckmarco@outlook.com>
2017-02-05 19:53:02 -0200

    Added a new Query type for all mongodb iteractions.

--- utils/mongodb/objects.go
+++ utils/mongodb/objects.go
@@ -4,4 +4,32 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+// M defines a generic type for mongodb queries.
 type M bson.M
+
+// Query defines an object to easily customise options to query functions
+type Query struct {
+	// Always required
+	Document string // Document to qury in
+	Find     M      // Query options
+
+	// Required for inserting and updating and optionaly for deleting
+	Data interface{}
+
+	// Required for single finds
+	Result interface{}
+
+	// Required for mutiple finds
+	Results []interface{}
+
+	// Optional
+	Limit int
+	Skip  int
+	Sort  []string
+	Hint  []string
+}
+
+// NewQuery returns a new Query object with Find and document initialized
+func NewQuery(document string, find M) (Query, error) {
+	return Query{Document: document, Find: find}, nil
+}
