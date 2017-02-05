6d9a230e8eaaef3f553eaae9713c5d3730fcf055
Marco Steck <steckmarco@outlook.com>
2017-02-04 22:38:15 -0200

    Add json and bson field options to every member of User struct

--- models/user/user.go
+++ models/user/user.go
@@ -7,7 +7,7 @@ import (
 )
 
 // TODO incorporate this into a dedicated util.
-type user struct {
+type User struct {
 	Name  string        `json:"name" bson:"name"`
 	Photo string        `json:"photo" bson:"photo"`
 	RA    string        `json:"ra" bson:"ra"`
@@ -18,82 +18,82 @@ type user struct {
 		Email    string `json:"email" bson:"email"`
 		Password []byte `json:"-" bson:"pwd"`
 		Salt     []byte `json:"-" bson:"salt"`
-	}
+	} `json:"account" bson:"account"`
 
 	Personal struct {
 		Birth        time.Time `json:"birth" bson:"birth"`
-		Phone        string    `json:"phone" bson:"phone"`
-		Drinks       int       `json:"drinks" bson:"drinks"`
+		Phone        string    `json:"phone,omitempty" bson:"phone,omitempty"`
+		Drinks       int       `json:"drinks,omitempty" bson:"drinks,omitempty"`
 		Relationship struct {
 			Sex    string `json:"sex" bson:"sex"`
-			Gender string `json:"gender" bson:"gender"`
-			Status string `json:"status" bson:"status"`
-		}
+			Gender string `json:"gender,omitempty" bson:"gender,omitempty"`
+			Status string `json:"status,omitempty" bson:"status,omitempty"`
+		} `json:"relationship" bson:"relationship"`
 		Origin struct {
 			Country string `json:"country" bson:"country"`
-			State   string `json:"state" bson:"state"`
-			City    string `json:"city" bson:"city"`
-			School  string `json:"school" bson:"school"`
-		}
-	}
+			State   string `json:"state,omitempty" bson:"state,omitempty"`
+			City    string `json:"city,omitempty" bson:"city,omitempty"`
+			School  string `json:"school,omitempty" bson:"school,omitempty"`
+		} `json:"origin" bson:"origin"`
+	} `json:"personal" bson:"personal"`
 
 	University struct {
 		Grad   string `json:"grad" bson:"grad"`
 		Reason struct {
-			Unicamp string `json:"unicamp" bson:"unicamp"`
-			Grad    string `json:"grad" bson:"grad"`
-		}
-	}
+			Unicamp string `json:"unicamp,omitempty" bson:"unicamp,omitempty"`
+			Grad    string `json:"grad,omitempty" bson:"grad,omitempty"`
+		} `json:"reason" bson:"reason"`
+	} `json:"university" bson:"university"`
 
 	Work struct {
 		Job     string `json:"job" bson:"job"`
 		Company string `json:"company" bson:"company"`
-	}
+	} `json:"work,omitempty" bson:"work,omitempty"`
 
-	Social map[string]string `json:"social" bson:"social"`
+	Social map[string]string `json:"social,omitempty" bson:"social,omitempty"`
 
 	Hobbies struct {
-		Languages []string `json:"languages" bson:"languages"`
+		Languages []string `json:"languages,omitempty" bson:"languages,omitempty"`
 
 		Games struct {
-			Favorite map[string]string `json:"favorite" bson:"favorite"`
-			Consoles map[string]string `json:"consoles" bson:"consoles"`
-			PC       bool              `json:"pc" bson:"pc"`
-			Extra    string            `json:"extra" bson:"extra"`
-			Accounts map[string]string `json:"accounts" bson:"accounts"`
-		}
+			Favorite map[string]string `json:"favorite,omitempty" bson:"favorite,omitempty"`
+			Consoles map[string]string `json:"consoles,omitempty" bson:"consoles,omitempty"`
+			PC       bool              `json:"pc,omitempty" bson:"pc,omitempty"`
+			Extra    string            `json:"extra,omitempty" bson:"extra,omitempty"`
+			Accounts map[string]string `json:"accounts,omitempty" bson:"accounts,omitempty"`
+		} `json:"games,omitempty" bson:"games,omitempty"`
 
 		Sports struct {
-			Plays []string `json:"plays" bson:"plays"`
-			Likes []string `json:"likes" bson:"likes"`
-			Teams []string `json:"teams" bson:"teams"`
-			Extra string   `json:"extra" bson:"extra"`
-		}
+			Plays []string `json:"plays,omitempty" bson:"plays,omitempty"`
+			Likes []string `json:"likes,omitempty" bson:"likes,omitempty"`
+			Teams []string `json:"teams,omitempty" bson:"teams,omitempty"`
+			Extra string   `json:"extra,omitempty" bson:"extra,omitempty"`
+		} `json:"sports,omitempty" bson:"sports,omitempty"`
 
 		TV struct {
-			Genres   []string          `json:"genres" bson:"genres"`
-			Movies   []string          `json:"movies" bson:"movies"`
-			Series   []string          `json:"series" bson:"series"`
-			Cartoons []string          `json:"cartoons" bson:"cartoons"`
-			Animes   []string          `json:"animes" bson:"animes"`
-			Accounts map[string]string `json:"accounts" bson:"accounts"`
-		}
+			Genres   []string          `json:"genres,omitempty" bson:"genres,omitempty"`
+			Movies   []string          `json:"movies,omitempty" bson:"movies,omitempty"`
+			Series   []string          `json:"series,omitempty" bson:"series,omitempty"`
+			Cartoons []string          `json:"cartoons,omitempty" bson:"cartoons,omitempty"`
+			Animes   []string          `json:"animes,omitempty" bson:"animes,omitempty"`
+			Accounts map[string]string `json:"accounts,omitempty" bson:"accounts,omitempty"`
+		} `json:"tv,omitempty" bson:"tv,omitempty"`
 
 		Programming struct {
-			Languages []string `json:"languages" bson:"languages"`
-			OS        []string `json:"languages" bson:"languages"`
-		}
+			Languages []string `json:"languages,omitempty" bson:"languages,omitempty"`
+			OS        []string `json:"languages,omitempty" bson:"languages,omitempty"`
+		} `json:"programming,omitempty" bson:"programming,omitempty"`
 
 		Music struct {
-			Genres []string `json:"genres" bson:"genres"`
-			Bands  []string `json:"bands" bson:"bands"`
-			Albuns []string `json:"albuns" bson:"albuns"`
-			Songs  []string `json:"songs" bson:"songs"`
-		}
-	}
+			Genres []string `json:"genres,omitempty" bson:"genres,omitempty"`
+			Bands  []string `json:"bands,omitempty" bson:"bands,omitempty"`
+			Albuns []string `json:"albuns,omitempty" bson:"albuns,omitempty"`
+			Songs  []string `json:"songs,omitempty" bson:"songs,omitempty"`
+		} `json:"music,omitempty" bson:"music,omitempty"`
+	} `json:"hobby,omitempty" bson:"hobby,omitempty"`
 
 	Friends []struct {
 		RA  string `json:"ra" bson:"ra"`
 		UID string `json:"uid" bson:"uid"`
-	}
+	} `json:"friends" bson:"friends"`
 }
